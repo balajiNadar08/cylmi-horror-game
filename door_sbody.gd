93b633e1daca53ed287548ae2d01176665bbdd60
Balaji Nadar <balajigittttt@gmail.com>
2026-07-22 01:54:26 +0530

    Door interaction changes

--- door_sbody.gd
+++ door_sbody.gd
@@ -1,5 +1,11 @@
 extends StaticBody3D
 
 func interact():
+	print("Door interacted")
+
+	var game = get_tree().current_scene.get_node("GameManager")
 	var ui = get_tree().current_scene.get_node("UI")
-	ui.show_dialog()
+
+	print(game.get_current_character())
+
+	ui.show_dialog(game.get_current_character())
