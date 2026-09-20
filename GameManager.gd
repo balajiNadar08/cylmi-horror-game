c764ea5af6dfabeb072ec51b52be329427e45d7b
Balaji Nadar <balajigittttt@gmail.com>
2026-09-20 21:42:46 +0530

    added new visitors

--- GameManager.gd
+++ GameManager.gd
@@ -2,17 +2,30 @@ extends Node
 
 var visitors = [
 	"deliveryman",
-	"shaman"
+	"police",
+	"stalker",
+	"electrician",
+	"shaman",
+	"mimic",
+	"mommy",
+	"neighbour",
 ]
 
 var current_visitor := 0
 
-func get_current_character():
+
+func get_current_character() -> String:
+	if current_visitor >= visitors.size():
+		return ""
+
 	return visitors[current_visitor]
 
+
 func next_character():
-	if current_visitor < visitors.size() - 1:
-		current_visitor += 1
-		print("Current visitor:", visitors[current_visitor])
+	current_visitor += 1
+
+	if current_visitor < visitors.size():
+		print("Next visitor:", visitors[current_visitor])
 	else:
 		print("All visitors completed!")
+		
