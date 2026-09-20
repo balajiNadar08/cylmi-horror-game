059aae73bc6a0f4cf2011835799181cea4b4003d
Balaji Nadar <balajigittttt@gmail.com>
2026-09-20 21:43:09 +0530

    minor changes for ui

--- door_sbody.gd
+++ door_sbody.gd
@@ -1,11 +1,21 @@
 extends StaticBody3D
 
 func interact():
-	print("Door interacted")
 
 	var game = get_tree().current_scene.get_node("GameManager")
 	var ui = get_tree().current_scene.get_node("UI")
 
-	print(game.get_current_character())
+	# Don't interact while dialogue is already open
+	if ui.is_dialog_open():
+		return
 
-	ui.show_dialog(game.get_current_character())
+	var character = game.get_current_character()
+
+	if character == "":
+		print("No more visitors.")
+		return
+
+	print("Door interacted")
+	print("Character:", character)
+
+	ui.show_dialog(character)
