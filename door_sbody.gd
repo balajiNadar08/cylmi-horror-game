2d8cee7cd4cb16cb14b64cae1d7834d79ba97173
Balaji Nadar <balajigittttt@gmail.com>
2026-09-21 20:53:37 +0530

    made changes for audio

--- door_sbody.gd
+++ door_sbody.gd
@@ -1,12 +1,19 @@
+
 extends StaticBody3D
+
 
 func interact():
 
 	var game = get_tree().current_scene.get_node("GameManager")
 	var ui = get_tree().current_scene.get_node("UI")
 
-	# Don't interact while dialogue is already open
+	# Don't interact while dialogue is open
 	if ui.is_dialog_open():
+		return
+
+	# Don't interact while waiting for a visitor
+	if not game.can_interact():
+		print("No visitor yet. Wait for the doorbell.")
 		return
 
 	var character = game.get_current_character()
@@ -17,5 +24,8 @@ func interact():
 
 	print("Door interacted")
 	print("Character:", character)
+
+	# Stop ringing when player opens the door
+	game.visitor_opened()
 
 	ui.show_dialog(character)
